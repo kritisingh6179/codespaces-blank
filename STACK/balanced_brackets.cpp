15c4e34e6140a85df8fded7a08db5594c38613cb
kriti singh <77061475+kritisingh6179@users.noreply.github.com>
2023-07-09 00:44:22 +0530

    balanced_brackets

--- STACK/balanced_brackets.cpp
+++ STACK/balanced_brackets.cpp
@@ -1,1 +1,33 @@
-
+//balanced paranthesis
+#include <bits/stdc++.h>
+using namespace std;
+bool balancebracket(string a){
+    stack<char> st;
+    for(int i=0;i<a.size();i++){
+        char ch=a[i];
+        if(ch=='(' or ch=='{' or ch=='['){
+            st.push(ch);
+            //cout<<ch<<endl;
+        }
+        else{
+            if(ch==')' and !st.empty() and st.top()=='('){
+                st.pop();
+            }
+            else if(ch=='}' and !st.empty() and st.top()=='{'){
+                st.pop();
+            }
+            else if(ch==']' and !st.empty() and st.top()=='['){
+                st.pop();
+            }else{
+                return false;
+            }
+        }
+        
+    }
+    return st.empty();
+}
+int main() {
+    string a="({})";
+    cout<<balancebracket(a);
+    return 0;
+}
